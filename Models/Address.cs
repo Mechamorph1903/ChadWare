d9080cac5ce1e61d04181808dd14cd027e74d75d
Daniel Anorue Jr. <danielanorue@gmail.com>
2025-04-22 15:16:07 -0500

    set up classes

--- Models/Address.cs
+++ Models/Address.cs
@@ -8,5 +8,13 @@ namespace ChadWare.Models
 {
     internal class Address
     {
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string Country { get; set; }
+        // Navigation property
+        public User User { get; set; }
     }
 }
--- Models/CartItem.cs
+++ Models/CartItem.cs
@@ -8,5 +8,18 @@ namespace ChadWare.Models
 {
     internal class CartItem
     {
+        public int Id { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalPrice => Product.Price * Quantity;
+        public CartItem(Product product, int quantity)
+        {
+            Product = product;
+            Quantity = quantity;
+        }
+        public void UpdateQuantity(int newQuantity)
+        {
+            Quantity = newQuantity;
+        }
     }
 }
