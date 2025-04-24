61bf1b6a2c02ea5c31d951f6629d380dd9c2baf3
Daniel Anorue Jr. <danielanorue@gmail.com>
2025-04-24 18:34:20 -0500

    setting up controllers for order and cart management

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ChadWare.Models;
+using ChadWare.Services;
 
 namespace ChadWare.Controllers
 {
     public class CartController
     {
+        readonly IDataService _data;
+        public CartController(IDataService dataService) => _data = dataService;
+
+        public Task<List<CartItem>> GetCartAsync(long userId)
+            => _data.GetCartAsync(userId);
+
+        public Task AddToCartAsync(long userId, CartItem item)
+            => _data.AddToCartAsync(userId, item);
+
+        public Task RemoveFromCartAsync(long userId, long cartItemId)
+            => _data.RemoveFromCartAsync(userId, cartItemId);
+
+        public async Task NavigateToCheckoutAsync(Page fromPage, long userId)
+        {
+            var items = await GetCartAsync(userId);
+            await fromPage.Navigation.PushAsync(
+                new Views.Pages.CheckoutPage(userId, items)
+            );
+        }
     }
-}
--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ChadWare.Models;
+using ChadWare.Services;
 
 namespace ChadWare.Controllers
 {
     public class OrderController
     {
+        readonly IDataService _data;
+        public OrderController(IDataService dataService) => _data = dataService;
+
+        public Task PlaceOrderAsync(Order order)
+            => _data.PlaceOrderAsync(order);
+
+        public async Task CompleteOrderAsync(Page fromPage, Order order)
+        {
+            await PlaceOrderAsync(order);
+            await fromPage.DisplayAlert("Success", "Your order has been placed!", "OK");
+            await fromPage.Navigation.PopToRootAsync();
+        }
     }
 }
