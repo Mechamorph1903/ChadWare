4ca1fdc1e608ce8a99076ec9d75f0e932d2b5a59
Daniel Anorue Jr. <danielanorue@gmail.com>
2025-04-24 20:58:58 -0500

    checkout logic

--- Views/Pages/CheckoutPage.xaml.cs
+++ Views/Pages/CheckoutPage.xaml.cs
@@ -1,9 +1,54 @@
-namespace ChadWare.Views;
+using Microsoft.Maui.Controls;
+using ChadWare.Controllers;
+using ChadWare.Models;
+using ChadWare.Services;
+using static System.Net.Mime.MediaTypeNames;
+using System.Collections.Generic;
+using System;
 
-public partial class CheckoutPage : ContentPage
+namespace ChadWare.Views.Pages
 {
-	public CheckoutPage()
-	{
-		InitializeComponent();
-	}
+    public partial class CheckoutPage : ContentPage
+    {
+        private readonly OrderController _orderController;
+        private long _currentUserId;
+        private IList<CartItem> _cartItems;
+
+        public CheckoutPage(long userId, IList<CartItem> items)
+        {
+            InitializeComponent();
+            var dataService = Application.Current.Services.GetService<IDataService>();
+            _orderController = new OrderController(dataService);
+
+            _currentUserId = userId;
+            _cartItems = items;
+
+            // pre-fill summary
+            CartSummary.ItemsSource = _cartItems;
+            TotalLabel.Text = _cartItems.Sum(i => i.Quantity * i.UnitPrice).ToString("C");
+        }
+
+        private async void OnPlaceOrderClicked(object sender, EventArgs e)
+        {
+            // gather shipping info from entry fields
+            var address = new Address
+            {
+                Street = StreetEntry.Text,
+                City = CityEntry.Text,
+                State = StateEntry.Text,
+                ZipCode = ZipEntry.Text
+            };
+
+            var order = new Order
+            {
+                UserID = _currentUserId,
+                TotalPrice = _cartItems.Sum(i => i.Quantity * i.UnitPrice),
+                OrderDate = DateTime.UtcNow,
+                Items = _cartItems.ToList(),
+                ShippingAddress = address
+            };
+
+            await _orderController.CompleteOrderAsync(this, order);
+        }
+    }
 }
