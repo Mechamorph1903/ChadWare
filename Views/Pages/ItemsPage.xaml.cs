7e8b635f2a3bf8a631196215480368b3c0cd9018
Kritika Upadhyay <60845749+kritiara@users.noreply.github.com>
2025-04-25 13:45:05 -0500

    Items search and add to cart added

--- Views/Pages/ItemsPage.xaml.cs
+++ Views/Pages/ItemsPage.xaml.cs
@@ -1,9 +1,54 @@
-namespace ChadWare.Views.Pages;
+using System.Collections.ObjectModel;
+using ChadWare.Models;
 
-public partial class ItemsPage : ContentPage
+namespace ChadWare.Views.Pages
 {
-	public ItemsPage()
-	{
-		InitializeComponent();
-	}
+    public partial class ItemsPage : ContentPage
+    {
+        public ObservableCollection<Product> Products { get; set; }
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            Products = new ObservableCollection<Product>
+            {
+                new Product { Name = "Ring", Price = 1299.00m, Image = "menapparel1.jpg" },
+                new Product { Name = "T-shirt", Price = 499.00m, Image = "menapparel2.jpg" },
+                new Product { Name = "Sunglasses", Price = 899.00m, Image = "menapparel3.jpg" },
+                new Product { Name = "Jeans", Price = 1599.00m, Image = "menapparel4.jpg" },
+                new Product { Name = "Jeans", Price = 1599.00m, Image = "menapparel5.jpg" },
+                new Product { Name = "Jeans", Price = 1599.00m, Image = "menapparel6.jpg" },
+                new Product { Name = "Jeans", Price = 1599.00m, Image = "menapparel7.jpg" },
+                new Product { Name = "Jeans", Price = 1599.00m, Image = "menapparel8.jpg" },
+            };
+
+            BindingContext = this;
+        }
+
+        private async void OnMenTapped(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MenProductPage());
+        }
+
+        private async void OnWomenTapped(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ProductPage());
+        }
+
+        private async void OnUserIconClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Coming Soon", "User profile or login screen will be here!", "OK");
+        }
+
+        private void OnAddToCartClicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            var product = button?.CommandParameter as Product;
+            if (product != null)
+            {
+                DisplayAlert("Cart", $"{product.Name} added to cart!", "OK");
+            }
+        }
+    }
 }
