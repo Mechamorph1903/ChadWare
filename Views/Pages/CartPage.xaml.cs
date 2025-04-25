fdf9b0c7256df4efed8d835704d75eab062e5af0
Daniel Anorue Jr. <danielanorue@gmail.com>
2025-04-24 20:14:54 -0500

    Revert "cart code behind"
    This reverts commit 5896e5fa62842cd0e4896108e358958fb2431ee4.

--- Views/Pages/CartPage.xaml.cs
+++ Views/Pages/CartPage.xaml.cs
@@ -1,50 +1,9 @@
-using ChadWare.Services;
-using ChadWare.Models;
-using ChadWare.Controllers;
-using System.Linq;
-using static System.Net.Mime.MediaTypeNames;
-using System;
+namespace ChadWare.Views;
 
-namespace ChadWare.Views.Pages
+public partial class CartPage : ContentPage
 {
-	public partial class CartPage : ContentPage
+	public CartPage()
 	{
-		private readonly CartController _cartController;
-		private long _currentUserId;
-
-        public CartPage()
-        {
-            InitializeComponent();
-
-            var dataService = Application.Current.Services.GetService<IDataService>();
-            _cartController = new CartController(dataService);
-
-            // Assume CurrentUser has been stored in App.Current.CurrentUser
-            _currentUserId = ((App)Application.Current).CurrentUser.UserID;
-        }
-        protected override async void OnAppearing()
-        {
-            base.OnAppearing();
-            // fetch cart items
-            var items = await _cartController.GetCartAsync(_currentUserId);
-            CartCollectionView.ItemsSource = items;
-        }
-
-        private async void OnRemoveClicked(object sender, EventArgs e)
-        {
-            if (sender is Button btn && btn.CommandParameter is CartItem item)
-            {
-                await _cartController.RemoveFromCartAsync(_currentUserId, item.CartItemID);
-                // refresh
-                CartCollectionView.ItemsSource = await _cartController.GetCartAsync(_currentUserId);
-            }
-        }
-
-        private async void OnCheckoutClicked(object sender, EventArgs e)
-        {
-            // navigate to checkout, passing cart items
-            await _cartController.NavigateToCheckoutAsync(this, _currentUserId);
-        }
-    }
+		InitializeComponent();
+	}
 }
-
