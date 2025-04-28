4f7efb2689694e41328f8f862f479401fb83a0ea
Kritika Upadhyay <60845749+kritiara@users.noreply.github.com>
2025-04-28 12:07:29 -0500

    added shessi's changes

--- Models/User.cs
+++ Models/User.cs
@@ -18,6 +18,7 @@ namespace ChadWare.Models
         public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
         public string PhoneNumber { get; set; } = string.Empty;
+        public DateTime LastLoginAt { get; internal set; }
 
         public User() { }
 
